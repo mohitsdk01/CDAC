254ae2a24316f09ff5524627b7860df2c3344acf
MohitK <mohitdk2@gmail.com>
2024-05-15 10:17:17 +0530

    update

---
+++ WPT/MYP/AJAX/JQueryAJAX/script.js
@@ -0,0 +1,27 @@
+// Calling JSON API using AJAX
+
+$(document).ready(function(){
+    function showData(result){
+        var str = "<table border='2'><tr><th>userId</th><th>id</th><th>title</th><th>completed</th></tr>"
+        for(let i in result){
+            str += `<tr><td>${i.userid}</td><td>${i.id}</td><td>${i.title}</td><td>${i.completed}</td></tr>`
+        }
+        str += "</table>"
+        $('#div').html(str);
+    };
+    $('#btn').click(function(){
+        $.ajax({
+            url: "https://jsonplaceholder.typicode.com/todos",
+            type:'POST',
+            async:true,
+            succes:function(result){
+                console.log(result.json());
+                showData(result.data);
+            },
+            error: function(err){
+                console.log(err);
+            }
+        })
+    })
+});
+// not working properly
